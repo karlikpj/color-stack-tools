4660b529b0bbb4d09b1b0e09b8f4f54546659470
karlikpj <paul.karlik@nih.gov>
2020-11-20 13:21:02 -0500

    update to consuming tokens and displaying tokens that are pointers

--- src/components/ColorChips.js
+++ src/components/ColorChips.js
@@ -13,7 +13,7 @@ import css from "../styles/styles.less";
 
 const ColorChips = (props) => {
   const { stack, id, isActive = false } = props;
-  const { dispatch } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
 
   const changeColor = (color) => {
     if (!isActive) return;
@@ -26,15 +26,12 @@ const ColorChips = (props) => {
     );
   };
 
-  const stackChip = (obj, index) => {
-    let color = obj.value || obj.default;
-    color = !color ? "--" : color;
-
-    const isColor = color.indexOf("#");
+  const makeColorChip = (obj) => {
+    const color = obj.value || "--";
     const name = obj.token;
     let lum = 0.0;
     let colorGrade = "";
-    if (isColor !== -1) {
+    if (color !== "--") {
       lum = luminance(...hexToRgb(color));
       colorGrade = grade(lum);
     }
@@ -46,14 +43,10 @@ const ColorChips = (props) => {
         key={`LT_${color}_${name}`}
       >
         <div className={css.chipHex}>
-          {isColor === -1 && color !== "--" && (
-            <div className={css.colortoken}>{name}</div>
-          )}
-
           <div
             className={css.chip}
             onClick={() => {
-              if (isColor === -1) return;
+              if (color === "--") return;
               changeColor(color, name);
             }}
             style={{ backgroundColor: color }}
@@ -65,6 +58,54 @@ const ColorChips = (props) => {
         </div>
       </li>
     );
+  };
+
+  const makePointerChip = (obj) => {
+    const { tokens } = state;
+    const { system } = tokens;
+    const color = obj.default || "--";
+    //const linkedColor = system.map((stack)=>{
+
+    //});
+    const name = obj.token;
+    let lum = 0.0;
+    let colorGrade = "";
+
+    return (
+      <li
+        className={`${css.stackItem} ${
+          colorGrade === "invalid" ? css.invalid : null
+        }`}
+        key={`LT_${color}_${name}`}
+      >
+        <div className={css.chipHex}>
+          <div className={css.colortoken}>{name}</div>
+          <div
+            className={css.chip}
+            onClick={() => {
+              if (color === "--") return;
+              changeColor(color, name);
+            }}
+            style={{ backgroundColor: color }}
+          ></div>
+          <div className={css.colortag}>{color}</div>
+          <div className={css.lumtag}>
+            {colorGrade !== "invalid" ? colorGrade : "--"}
+          </div>
+        </div>
+      </li>
+    );
+  };
+
+  const stackChip = (obj) => {
+    let color = obj.value || "--";
+    let hasDefault = obj?.default;
+    const notColor = color.indexOf("#");
+
+    if (!notColor && !hasDefault) {
+      return makeColorChip(obj);
+    }
+    if (notColor && hasDefault) return makePointerChip(obj);
   };
 
   const colorStack = stack.map((color, index) => {
